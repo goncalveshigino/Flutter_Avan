4be0fab7ac5f38426a0b6e36ff28cb1c69645735
goncalveshigino <goncalveshigino20@gmail.com>
2022-05-05 01:22:11 +0100

    chore add helpers

--- rutasapp/lib/main.dart
+++ rutasapp/lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:rutasapp/pages/acesso_gps_page.dart';
+import 'package:rutasapp/pages/loading_page.dart';
+import 'package:rutasapp/pages/mapa_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: const Text('Hello World'),
-          ),
-        ),
-      ),
+      home: const LoadingPage(),
+      routes: {
+        'mapa': (_) => const MapaPage(),
+        'loading': (_) => const LoadingPage(),
+        'acesso_gsp': (_) => const AcessoGpsPage()
+      },
     );
   }
 }
--- rutasapp/lib/pages/loading_page.dart
+++ rutasapp/lib/pages/loading_page.dart
@@ -1,14 +1,28 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:rutasapp/helpers/helpers.dart';
+import 'package:rutasapp/pages/mapa_page.dart'; 
 
 class LoadingPage extends StatelessWidget {
   const LoadingPage({ Key key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       body: Center(
-         child: Text('LoadingPage'),
-       ),
+    return  Scaffold(
+       body: FutureBuilder(
+         future: checkGpsLocation(context),
+         builder: (BuildContext context, AsyncSnapshot snapshot){
+            return const Center(
+              child: CircularProgressIndicator(strokeWidth: 2),
+            );
+         }
+       )
     );
   }
+
+   Future checkGpsLocation(BuildContext context) async{
+
+
+     await Future.delayed(const Duration( seconds: 4));
+     Navigator.pushReplacementNamed(context, navegarMapaFadeIn(context, const MapaPage() ) );
+   }
 }
